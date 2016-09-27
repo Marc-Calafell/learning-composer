004eb5c747385f38e477e870e2b7ec2d5c3b3122
Marc <mcalafellsmx@gmail.com>
2016-09-27 17:26:27 +0200

    generating conflict

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+
+
 require "vendor/autoload.php";
+require "src/SayHello.php";
+
 
 $sayhello= new \HelloWorld\SayHello();
 
 echo $sayhello->world();
 
+
+$sayhellw= new SayHello();
+
+echo $sayhellw->world();
 //echo $sayhello;
---
+++ src/SayHello.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mark
+ * Date: 27/09/16
+ * Time: 17:18
+ */
+class SayHello
+{
+    public static function world()
+    {
+
+        return "Hola mon";
+
+
+    }
+
+
+}
